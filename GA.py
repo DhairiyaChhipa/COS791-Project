7fe7764b516f41b669caa63214d14d3e8aa4a370
Dhairiya Chhipa <dhairiyac@gmail.com>
2024-10-23 16:16:03 +0200

    Updated normal GA with tourney selection

--- GA.py
+++ GA.py
@@ -7,11 +7,11 @@ from Kapur import Kapur
 class Constants(Enum):
     CROSSOVER_RATE = 0.7
     MUTATION_RATE = 0.3
-    POPULATION_SIZE = 25
     GENERATIONS = 100
+    POPULATION_SIZE = 26
+    ELITIST_SIZE = 4
     TOURNAMENT_SIZE = 5
-    # SELECTION_SIZE = 5  # quarter of population size (20 / 2 -> [10] / 2 -> [5])
-    ELITIST_SIZE = 5
+    SELECTION_SIZE = (POPULATION_SIZE / 2) + 1
     MUTATION_STRATEGY = 0  # 0 = +- 10, 1 = random int (1, 254)
 
 
@@ -23,45 +23,52 @@ class GeneticAlgorithm:
         self._kapur = kapur
 
     # def __del__(self):
-    #     for _ in range(int(Constants.POPULATION_SIZE.value)):
-    #         item = self._generation.pop()
-    #         del item
-    # 
+    #     # print("Constants.POPULATION_SIZE.value", Constants.POPULATION_SIZE.value)
+    #     # print("len:", len(self._generation))
+
+    #     for i in range(int(Constants.POPULATION_SIZE.value)):
+    #         del self._generation[i]
+    
     #     if len(self._generation) == 0:
     #         del self._generation
 
     def start(self):
-        for x in range(int(Constants.POPULATION_SIZE.value)):  # initialise generation
+        for _ in range(int(Constants.POPULATION_SIZE.value)):  # initialise generation
             self._generation.append(Chromosome(self._kapur, self._threshold_count))
+
+        bestIndividual = None
         for _ in range(int(Constants.GENERATIONS.value)):
             self.propagate()
-            print("best fitness: ", self.getBest().fitness)
+            bestIndividual = self.getBest(self._generation, int(Constants.POPULATION_SIZE.value))
+            print("best fitness: ", bestIndividual.fitness)
 
-        return self.getBest()
+        return bestIndividual
 
     def propagate(self):  # handles selection and repopulation
-        newGeneration = self.selectIndividuals()
-        for _ in range(int((Constants.POPULATION_SIZE.value - Constants.ELITIST_SIZE.value) / 2)):
+        tournamentGeneration = self.tournamentSelection()
+        newGeneration = []
 
-            parent1 = np.random.randint(0, int(Constants.POPULATION_SIZE.value - 1))
-            parent2 = np.random.randint(0, int(Constants.POPULATION_SIZE.value - 1))
-            child1Thresholds, child2Thresholds = self._generation[parent1].copyThresholds(), self._generation[parent2].copyThresholds()
-
-            while not (parent2 == parent1):
-                parent2 = np.random.randint(0, int(Constants.POPULATION_SIZE.value - 1))
+        for i in range(int(Constants.SELECTION_SIZE.value) - 1):
+            parent1 = i
+            parent2 = i + 1
+            child1Thresholds, child2Thresholds = tournamentGeneration[parent1].copyThresholds(), tournamentGeneration[parent2].copyThresholds()
 
             if (np.random.randint(0, 1)) < Constants.CROSSOVER_RATE.value:  # check if crossover can be done
-                child1Thresholds, child2Thresholds = self.crossover(self._generation[parent1], self._generation[parent2])
+                child1Thresholds, child2Thresholds = self.crossover(tournamentGeneration[parent1], tournamentGeneration[parent2])
 
             if (np.random.randint(0, 1)) < Constants.MUTATION_RATE.value:  # check if mutation can be done
                 child1Thresholds = self.mutation(child1Thresholds)
                 child2Thresholds = self.mutation(child2Thresholds)
+
             child1Thresholds.sort()
             child2Thresholds.sort()
+
             child1 = Chromosome(self._kapur, thresholds=child1Thresholds)
             child2 = Chromosome(self._kapur, thresholds=child2Thresholds)
+
             newGeneration.append(child1)
             newGeneration.append(child2)
+
         self.repopulate(newGeneration)
 
     def crossover(self, chromosome1: Chromosome, chromosome2: Chromosome):
@@ -75,10 +82,12 @@ class GeneticAlgorithm:
 
         return childThresholds1, childThresholds2
 
+    def ILS(self, list: list):
+        return None
+
     def mutation(self, thresholds: list):
         # Random mutation
         childThresholds = thresholds
-        # print(len(chromosome.thresholds))
         index = np.random.randint(0, len(thresholds))
 
         if Constants.MUTATION_STRATEGY.value == 0:
@@ -88,16 +97,28 @@ class GeneticAlgorithm:
             childThresholds[index] = np.random.randint(1, 254)
 
         return childThresholds
+    
+    def tournamentSelection(self):
+        bestIndividuals = []
+        selectionCounter = 0
 
-    def selectIndividuals(self):
-        selectedPopulation = []
-        self.reorderPopulation()
+        while (selectionCounter < int(Constants.SELECTION_SIZE.value)):
+            tournament = []
+            tournamentCounter = 0
 
-        for x in range(int(Constants.ELITIST_SIZE.value)):  # elitism, get the best chromosomes from population
-            selectedPopulation.append(
-                Chromosome(self._kapur, thresholds=self._generation[x].thresholds, fitness=self._generation[x].fitness))
+            while (tournamentCounter < int(Constants.TOURNAMENT_SIZE.value)):
+                randomIndividual = self._generation[np.random.randint(0, len(self._generation))]
+                if randomIndividual not in tournament:
+                    tournamentCounter += 1
+                    tournament.append(randomIndividual)
+            
+            bestIndividual = self.getBest(tournament, len(tournament))
 
-        return selectedPopulation
+            if bestIndividual not in bestIndividuals:
+                selectionCounter += 1
+                bestIndividuals.append(Chromosome(self._kapur, thresholds=bestIndividual.thresholds, fitness=bestIndividual.fitness))
+
+        return bestIndividuals
 
     def repopulate(self, newGeneration):
         for x in range(int(Constants.POPULATION_SIZE.value)):
@@ -106,17 +127,22 @@ class GeneticAlgorithm:
     def compareFitness(self, x: Chromosome, y: Chromosome):
         return x.fitness > y.fitness
 
-    def reorderPopulation(self):
-        for i in range(int(Constants.POPULATION_SIZE.value)):
+    def reorderList(self, list, size):
+        for i in range(size):
             swapped = False
 
-            for j in range(int(Constants.POPULATION_SIZE.value) - i - 1):
-                if self.compareFitness(self._generation[j + 1], self._generation[j]):
-                    self._generation[j], self._generation[j + 1] = self._generation[j + 1], self._generation[j]
+            for j in range(size - i - 1):
+                if self.compareFitness(list[j + 1], list[j]):
+                    list[j], list[j + 1] = list[j + 1], list[j]
                     swapped = True
 
             if not swapped:
                 break
 
-    def getBest(self):
-        return self._generation[0]
+    def getBest(self, list, size):
+        best = list[0]
+        for i in range(1, size):
+            if self.compareFitness(list[i], best):
+                best = list[i]
+
+        return best
